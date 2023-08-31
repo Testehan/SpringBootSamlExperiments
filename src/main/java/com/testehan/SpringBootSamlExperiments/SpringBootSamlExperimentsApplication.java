a46772f2416c7715aec46dbfbc5e7ff226babf52
DanTestehan <tdan89@yahoo.com>
2023-08-31 20:19:19 +0100

    main method class

---
+++ src/main/java/com/testehan/SpringBootSamlExperiments/SpringBootSamlExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.testehan.SpringBootSamlExperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSamlExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSamlExperimentsApplication.class, args);
+	}
+
+}
