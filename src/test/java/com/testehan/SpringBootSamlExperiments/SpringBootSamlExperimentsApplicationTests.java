23cf9d597d674c70896c27cd6112a8f6c8cb322b
DanTestehan <tdan89@yahoo.com>
2023-08-31 20:19:19 +0100

    empty test

---
+++ src/test/java/com/testehan/SpringBootSamlExperiments/SpringBootSamlExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testehan.SpringBootSamlExperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSamlExperimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
